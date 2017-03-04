38bab0bf0cc942e0b81e6eafd510feb7ef8a7c76
htter668 <htter668@gmail.com>
2017-03-04 15:48:04 +0800

    交作業

---
+++ Data/Lab2/Lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\..\Input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (!char.IsWhiteSpace(ch))
+                        {
+                            if(char.IsPunctuation(ch))
+                            {
+                                if(ch=='.')
+                                    Console.Write(ch+"\n");
+                                else if(ch=='?')
+                                    Console.Write(ch + "\n");
+                                else if(ch=='!')
+                                    Console.Write(ch + "\n");
+                                else
+                                    Console.Write(ch);
+                            }   
+                            else
+                                Console.Write(ch);
+                        }
+                        else
+                            Console.Write(ch);
+                    }
+                }
+            }
+        }
+    }
+}
